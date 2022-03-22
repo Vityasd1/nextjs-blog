57331fdf816731647aefa5a3801845a72df896d7
vmaria <vmaria@combit.hu>
2022-03-22 16:18:47 +0100

    Post modosítása, ujratöltés miatt

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import {useRouter} from "next/router";
 import {getPosts, getPostDetails } from "../../services";
-import {PostDetail, PostWidget, Author, Comments, CommentsForm, Categories} from "../../components";
+import {PostDetail, PostWidget, Author, Comments, CommentsForm, Categories, Loader} from "../../components";
 
 const PostDetails = ({post}) => {
+    const router = useRouter();
+    if(router.isFallback){
+        return <Loader/>
+    }
+
     return (
         <>
             <div className="container mx-auto px-10 mb-8">
